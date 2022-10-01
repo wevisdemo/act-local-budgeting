ba33c68bca8e4616d9b85f5be10eef40df3c460f
mixth <mixth@users.noreply.github.com>
2022-10-01 19:50:16 +0000

    deploy: 9b2450e91e7bfb27defca9ab9762499e9d8b9630

--- _nuxt/static/1664619875/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fconclusion","\u002Fdashboard","\u002Fstory"]})
---
+++ _nuxt/static/1664653550/conclusion/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fact-local-budgeting\u002F_nuxt\u002Fstatic\u002F1664653550",layout:"default",error:a,serverRendered:true,routePath:"\u002Fconclusion",config:{_app:{basePath:"\u002Fact-local-budgeting\u002F",assetsPath:"\u002Fact-local-budgeting\u002F_nuxt\u002F",cdnURL:a}}}}(null));
