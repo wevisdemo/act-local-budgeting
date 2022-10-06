ba14220698b927a8eca62156c72ee302a341563a
Narat S <me@mixth.net>
2022-10-07 00:37:01 +0700

    Add province budget per capita to nation-wide

--- scripts/data-fetcher/generate/nation-wide/nation-wide.ts
+++ scripts/data-fetcher/generate/nation-wide/nation-wide.ts
@@ -1,26 +1,58 @@
-import { BudgetRow } from "../../read/types"
-import { NationWideBudget } from "./nation-wide.types"
-import { getUnique } from "../utils"
+import { BudgetRow, ByYearLinkRow } from "../../read/types"
+import { NationWideBudget, ProviceBudgetPerCapita } from "./nation-wide.types"
+import { getUnique, sum } from "../utils"
 import { getGroupedByArea, getGroupedByType } from "../group-budget"
+import { logc } from "../../log-collector/log-collector"
 
 export type NationWideFileData = { year: number, budget: NationWideBudget }
 
-export function generateNationWide(budgets: BudgetRow[]): NationWideFileData[] {
+export function generateNationWide(
+  budgets: BudgetRow[],
+  byYearLinks: ByYearLinkRow[],
+  ): NationWideFileData[] {
+    const years = getUnique(budgets.map(row => row.year))
   const expenseRows = budgets.filter(row => row.type === 'expense')
-  const years = getUnique(expenseRows.map(row => row.year))
+  const incomeRows = budgets.filter(row => row.type === 'income')
 
   return years.map(year => {
-    const filteredBudgets = expenseRows.filter(row => row.year === Number(year))
-    return { year, budget: getNationWide(filteredBudgets) }
+    const yearScopedBudgets = expenseRows.filter(row => row.year === year)
+    const yearScopedIncomes = incomeRows.filter(row => row.year === year)
+    const yearScopedByYearLinks = byYearLinks.filter(row => row.year === year)
+    const budgetPerCapitas = getBudgetPerCapitas(yearScopedIncomes, yearScopedByYearLinks, year)
+
+    return { year, budget: getNationWide(yearScopedBudgets, budgetPerCapitas) }
   })
 }
 
-function getNationWide(filteredBudgets: BudgetRow[]): NationWideBudget {
+function getBudgetPerCapitas(
+  incomes: BudgetRow[],
+  yearByYearLinks: ByYearLinkRow[],
+  year: number,
+  ): ProviceBudgetPerCapita[] {
+  const provinces = getUnique(incomes.map(row => row.province))
+  const budgetPerCapitas = provinces
+    .map(province => {
+      const population = yearByYearLinks.find(row => row.province === province)?.population
+      logc.warn(`Generate Nation-wide: cannot find population info for ${province} - ${year}, skipping its BudgetPerCapita`, { source: 'ByYearLink' })
+      if (population === undefined) {
+        return undefined
+      }
+
+      const budget = incomes
+        .filter(row => row.province === province)
+        .map(row => row.amount)
+        .reduce(sum, 0)
+      return { name: province, amount: budget / population }
+    })
+  return budgetPerCapitas.filter(e => e !== undefined) as ProviceBudgetPerCapita[]
+}
+
+function getNationWide(filteredBudgets: BudgetRow[], budgetPerCapita: ProviceBudgetPerCapita[]): NationWideBudget {
   return {
     total: filteredBudgets
       .reduce((sum, row) => sum + row.amount, 0),
     groupedByArea: getGroupedByArea(filteredBudgets),
     groupedByType: getGroupedByType(filteredBudgets),
-    budgetPerCapita: [],
+    budgetPerCapita,
   }
 }
