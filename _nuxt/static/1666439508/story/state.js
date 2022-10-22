f3dd298567d7d72fcf96071f93a97c0131e6ce6b
Pimpatipan <Pimpatipan@users.noreply.github.com>
2022-10-22 22:23:10 +0000

    deploy: ab172c54adb089a57724b743e0bde29f85d04c57

--- _nuxt/static/1666439508/story/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fact-local-budgeting\u002F_nuxt\u002Fstatic\u002F1666439508",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstory",config:{_app:{basePath:"\u002Fact-local-budgeting\u002F",assetsPath:"\u002Fact-local-budgeting\u002F_nuxt\u002F",cdnURL:a}}}}(null));
